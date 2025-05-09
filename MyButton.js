2c8facb08b63cf81a81ed3b7a2808d540004f1a1
Jaskaran Singh <89383365+Jaskaran-Techno@users.noreply.github.com>
2025-05-09 23:04:40 +0530

    Create MyButton.js

---
+++ MyButton.js
@@ -0,0 +1,19 @@
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update separately</h1>
+      <MyButton />
+      <MyButton />
+    </div>
+  );
+}
+
+function MyButton() {
+  // ... we're moving code from here ...
+}
