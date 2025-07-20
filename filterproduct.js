7e879ae61fa231ef5a9167e976319b706d492a96
Jaskaran Singh <89383365+Developer-Ninjaa@users.noreply.github.com>
2025-07-20 19:25:24 +0530

    Create filterproduct.js

---
+++ filterproduct.js
@@ -0,0 +1,11 @@
+function FilterableProductTable({ products }) {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar 
+        filterText={filterText} 
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly} />
